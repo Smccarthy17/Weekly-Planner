54488a2f67cf35dea5351acaa7d9731cc7f82538
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-05-13 15:14:14 -0400

    added colorCodedTimeblocks and eventListener

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,20 @@
 var textAreaSections = document.querySelectorAll("textarea");
+var saveBtns = document.querySelectorAll(".saveBtn");
+console.log(saveBtns);
 var currentDateSection = document.getElementById("currentDay");
 currentDateSection.textContent = moment().format("dddd, MMMM Do");
-console.log(moment().hour());
+
 for (var i = 0; i < textAreaSections.length; i++) {
-  console.log(textAreaSections[i].id);
   if (textAreaSections[i].id < moment().hour()) {
     textAreaSections[i].classList.add("past");
+  } else if (textAreaSections[i].id == moment().hour()) {
+    textAreaSections[i].classList.add("present");
+  } else {
+    textAreaSections[i].classList.add("future");
   }
+}
+for (var i = 0; i < saveBtns.length; i++) {
+  saveBtns[i].addEventListener("click", function () {
+    console.log(this);
+  });
 }
