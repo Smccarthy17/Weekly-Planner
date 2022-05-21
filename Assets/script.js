cc5baf3b092f6d19ce16eddd0b27684d6981c336
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-05-20 23:15:50 -0400

    connect siblings add local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,23 @@
 var textAreaSections = document.querySelectorAll("textarea");
 var saveBtns = document.querySelectorAll(".saveBtn");
-console.log(saveBtns);
 var currentDateSection = document.getElementById("currentDay");
 currentDateSection.textContent = moment().format("dddd, MMMM Do");
+
+// console.log(localStorage.getItem("8"))
+// var save = document.getElementById("8")
+// console.log(save)
+// save.value = localStorage.getItem("8")
+
+document.getElementById("8").value = localStorage.getItem("8")
+document.getElementById("9").value = localStorage.getItem("9")
+document.getElementById("10").value = localStorage.getItem("10")
+document.getElementById("11").value = localStorage.getItem("11")
+document.getElementById("12").value = localStorage.getItem("12")
+document.getElementById("13").value = localStorage.getItem("13")
+document.getElementById("14").value = localStorage.getItem("14")
+document.getElementById("15").value = localStorage.getItem("15")
+document.getElementById("16").value = localStorage.getItem("16")
+document.getElementById("17").value = localStorage.getItem("17")
 
 for (var i = 0; i < textAreaSections.length; i++) {
   if (textAreaSections[i].id < moment().hour()) {
@@ -16,5 +31,15 @@ for (var i = 0; i < textAreaSections.length; i++) {
 for (var i = 0; i < saveBtns.length; i++) {
   saveBtns[i].addEventListener("click", function () {
     console.log(this);
+    console.log(this.previousElementSibling)
+    console.log(this.previousElementSibling.value)
+    console.log(this.previousElementSibling.id)
+
+    var time = this.previousElementSibling.id
+    var userValue = this.previousElementSibling.value
+    localStorage.setItem(time, userValue)
   });
 }
+
+
+
